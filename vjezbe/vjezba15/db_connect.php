185552129d0f6c58b3f9819374d616ffd2643545
xyz123-jpg <73470554+xyz123-jpg@users.noreply.github.com>
2024-11-07 10:47:58 +0100

    Update db_connect.php

--- vjezbe/vjezba15/db_connect.php
+++ vjezbe/vjezba15/db_connect.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Zadano korisničko ime u XAMPP-u
-$password = ""; // Zadana lozinka je prazna (bez lozinke)
-$database = "ntpwp"; // Zamijenite 'your_database_name' s nazivom vaše baze
+$username = "root"; 
+$password = ""; 
+$database = "ntpwp"; 
 
-// Kreiranje konekcije
+
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Provjera konekcije
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
