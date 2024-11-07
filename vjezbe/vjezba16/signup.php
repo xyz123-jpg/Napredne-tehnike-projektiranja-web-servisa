56e787df99266496bd9ebcefe32feaace83d977f
xyz123-jpg <73470554+xyz123-jpg@users.noreply.github.com>
2024-11-07 10:49:11 +0100

    Update signup.php

--- vjezbe/vjezba16/signup.php
+++ vjezbe/vjezba16/signup.php
@@ -1,8 +1,8 @@
 <?php
-// Uključite datoteku s konekcijom
-include('db_connect.php'); // Datoteka koja sadrži konekciju s bazom podataka
 
-// Provjerite je li forma poslana
+include('db_connect.php'); 
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = htmlspecialchars($_POST['name']);
     $lastname = htmlspecialchars($_POST['lastname']);
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $password = htmlspecialchars($_POST['password']);
     $country_code = htmlspecialchars($_POST['country_code']);
 
-    // Provjera da li korisničko ime ili email već postoji
+
     $sql = "SELECT * FROM users WHERE username = ? OR email = ?";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "ss", $username, $email);
@@ -21,15 +21,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (mysqli_num_rows($result) > 0) {
         echo "Korisničko ime ili email već postoji.";
     } else {
-        // Hashiranje lozinke
+
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-        // SQL upit za umetanje novog korisnika
         $sql = "INSERT INTO users (name, lastname, email, username, password, country_code, created, modified) VALUES (?, ?, ?, ?, ?, ?, 1, 0)";
         $stmt = mysqli_prepare($conn, $sql);
         mysqli_stmt_bind_param($stmt, "ssssss", $name, $lastname, $email, $username, $hashed_password, $country_code);
 
-        // Izvršite upit
+
         if (mysqli_stmt_execute($stmt)) {
             echo "Uspješno ste se registrirali!";
         } else {
@@ -37,10 +36,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 
-    // Zatvorite pripremljenu izjavu
     mysqli_stmt_close($stmt);
 }
 
-// Zatvorite konekciju
 mysqli_close($conn);
 ?>
