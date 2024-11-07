1d1461094e2fca905f35eb576c21299826fa9aa1
xyz123-jpg <73470554+xyz123-jpg@users.noreply.github.com>
2024-11-07 10:52:17 +0100

    Update update_user.php

--- vjezbe/vjezba18/update_user.php
+++ vjezbe/vjezba18/update_user.php
@@ -1,15 +1,14 @@
 <?php
-// Uključivanje konekcije na bazu
+
 include 'db_connect.php';
 
-// Provjerava da li su podaci poslani
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $user_id = intval($_POST['id']);
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
     $country_id = intval($_POST['country_id']);
 
-    // SQL upit za ažuriranje korisničkih podataka
     $query = "UPDATE users SET name = '$name', lastname = '$lastname', country_id = $country_id WHERE id = $user_id";
     
     if (mysqli_query($conn, $query)) {
@@ -21,6 +20,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo "Nisu svi podaci poslani.";
 }
 
-// Zatvaranje konekcije
 mysqli_close($conn);
 ?>
